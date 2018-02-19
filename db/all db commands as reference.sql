e5e19d54e2b5940697a4fd161231dd9baf610192
pcdavis <pcdavis@users.noreply.github.com>
2018-02-19 09:49:00 -0700

    first commit to new branch

--- db/all db commands as reference.sql
+++ db/all db commands as reference.sql
@@ -1,14 +1,40 @@
-create TABLE stacks(
-    stack_id serial primary key,
-    stack_title text,
-    user_id INTEGER REFERENCES users(user_id)
-)
 
+--Create a stack
 insert into stacks
 (stack_title, user_id)
 VALUES
 ('My Second Stack', 1)
 returning *;
+
+--Create quiz questions
+insert into quizes 
+(user_id, question, correct_answer, false_1, false_2, false_3)
+VALUES 
+(1, 'Is the world flat?', 'No', 'Yes', null, null)
+returning *;
+
+--User these to know what values to use in the next insert into stack_content
+SELECT * from stacks
+SELECT * from quizes 
+
+--This is needed to assign the quizes to specific stacks
+insert into stack_content (stack_id,quiz_id,user_id)
+VALUES (5,5,1)
+returning *;
+
+-- The following query will select all stacks and their content from a specific user
+select * from stacks
+join quizes
+on stacks.user_id = quizes.user_id 
+where stacks.user_id = 1
+
+
+-- Extra Shit --------------------
+create TABLE stacks(
+    stack_id serial primary key,
+    stack_title text,
+    user_id INTEGER REFERENCES users(user_id)
+)
 
 CREATE TABLE quizes(
     quiz_id serial primary key,
@@ -19,19 +45,21 @@ CREATE TABLE quizes(
     false_2 text,
     false_3 text
 )
-insert into quizes 
-(user_id, question, correct_answer, false_1, false_2, false_3)
-VALUES 
-(1, 'Is it true?', 'Yes', 'No', null, null)
-returning *;
 
+CREATE TABLE broadcast(
+    broadcast_id serial primary key,
+    user_id INTEGER REFERENCES users(user_id),
+    stack_id INTEGER REFERENCES stacks(stack_id),
+    broadcast_code text,
+    broadcast_start_time INTEGER,
+    broadcast_end_time INTEGER
+)
 
 
 select * from quizes 
 join stacks
 on quizes.user_id = stacks.user_id
 where stack_id = 4
-
 
 CREATE TABLE stack_content(
     content_id serial primary key,
@@ -40,28 +68,3 @@ CREATE TABLE stack_content(
     quiz_id INTEGER REFERENCES quizes(quiz_id),
     user_id INTEGER REFERENCES users(user_id)
 )
-SELECT * from stacks
-SELECT * from quizes
-
-insert into stack_content (stack_id,quiz_id,user_id)
-VALUES (5,5,1)
-returning *;
-
-
-
-select * from quizes
-join stack_content
-on quizes.quiz_id = stack_content.quiz_id
-where stack_id = 1
-
-
-select * from stacks
-join quizes
-on stacks.user_id = quizes.user_id 
-where stack_id = 1
-
--- The following query will select all stacks and their content from a specific user
-select * from stacks
-join quizes
-on stacks.user_id = quizes.user_id 
-where stacks.user_id = 1
--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -8,40 +8,25 @@ import { Form, FormControl, Button } from 'react-bootstrap'
 
 
 class Stack extends Component {
-    // constructor(props){
-    //   super(props);
-    //   this.state = {
-    //     new_stack_title: '',
-    //     new_
-    //   }
-    //   this.handleChange = this.handleChange.bind(this)
-    // }
+  constructor(props){
+    super(props);
+    this.state = {
+      broadcast_code: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  
 
   componentDidMount() {
- 
-    console.log('inside Stack component in component did mount this.props.stacks',this.props.stacks)
-
-    // let this_stack = _.filter(this.props.stacks, this.props.stacks.stack_id === stackID);
-    // let allStackContents = this.props.stacks;
-    // let this_stack = _.mapValues(allStackContents, function(o) { 
-    //   if(o.stack_id === stackID) {
-    //     return o;
-    //   }   });
-    // let this_stack =  _.filter( this.props.stacks, {stack_id: stackID})
-    // let this_stack =  _.filter( this.props.stacks, {stack_id: stackID})
-    // let this_stack =  this.props.stacks.filter( stackItem => {
-    //   return stackItem.stack_id === stackID
-    // })
-
- 
-    // console.log('this_stack',this_stack)
-    
+     console.log('inside Stack component in component did mount this.props.stacks',this.props.stacks)
   }
 
   renderStackItems() {
     let stackID = this.props.match.params.id;
     console.log('this.props.match.params.id',this.props.match.params.id)
     console.log('stackID is equal to this.props.match.params.id',stackID)
+
+    
     
     return _.map(this.props.stacks, stack => {
       console.log("mapping in the stack page the stack item with stack id of------------------- ", stack.stack_id)
@@ -58,43 +43,37 @@ class Stack extends Component {
     });
   }
 
-  // renderStacksItems() {
-
-  // return _.map(this.props.stacks.stack)
-  //   let stackKeys = Object.keys(this.props.stacks);
-  //   console.log('stackKeys',stackKeys)
-    
-  //   return _.map(this.props.stacks.stackItems, stackItem => {
-  //     return (
-  //       <li className="list-group-item" key={stack.stack_id}>
-  //         <Link to={`/stacks/${stack.stack_id}`}>
-  //           {stack.stack_title}
-  //         </Link>
-  //       </li>
-  //     );
-  //   });
-  // }
 
 
 
-// handleChange(event){
-//   this.setState({
-//     title: event.target.value
-//   })
-// }
+handleChange(event){
+  this.setState({
+    broadcast_code: event.target.value
+  })
+}
 
   render() {
+    const stackTitle = this.props.stacks.stack_title;
+
     
     // let stackID = this.props.match.params.id;
     // const {stack_title,question,correct_answer, false_1, false_2, false_3} = this.props.stacks[stackID];
     return (
       <div>
       
-        <h1> Hello to the Stack</h1>
+        <h1> Welcome to your stack</h1>
+        <h1> {this.props.stacks.stack_title} </h1>
         {this.renderStackItems()}
 
         <div>
+        <Form inline>
+        <FormControl 
+        placeholder = 'Broadcast Code'
+        onChange = { event => this.setState({ title: event.target.value})}
+        />
+        
         <Button onClick={() => console.log("start broadcast")}>Start Broadcast</Button>
+      </Form>
         </div>
       </div>
     );
