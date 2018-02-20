5fa990dc1d5f2d8a36ef7e6276471b184361a4f4
pcdavis <pcdavis@users.noreply.github.com>
2018-02-20 12:39:02 -0700

    second commit

--- src/components/Classroom/Classroom.js
+++ src/components/Classroom/Classroom.js
@@ -74,8 +74,10 @@ class SocketRoom extends Component {
     uiToDisplay () {
         if(this.state.isTeacher){
             return (
-                <h1>The Teacher UI </h1>     
-                <h1>Now on feb20_socekt_feature git branch </h1>     
+              <div>
+                    <h1>The Teacher UI </h1>     
+                    <h1>Now on feb20_socekt_feature git branch </h1>     
+              </div>
             );
         }
         return (
