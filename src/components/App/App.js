e7e0b8c0f4594c1a1cb531ebe0e3b9c48b0ec4d6
pcdavis <pcdavis@users.noreply.github.com>
2018-07-30 14:43:54 -0400

    cleanup: removed app.css from App.js (placed it in archive folder)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import routes from '../../routes'
 
 class App extends Component {
@@ -13,31 +12,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-//Backup of original App component
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-
-//         <h1>This is the App component</h1>
-//                 <a href={ process.env.REACT_APP_LOGIN }><button>Login/Register</button></a>
-
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
